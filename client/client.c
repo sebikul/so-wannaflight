621f971a1563df7bc4d06df60aab871711196938
Sebastian Kulesz <sebikul@gmail.com>
2015-09-10 01:50:27 -0300

    Cliente y servidor realizan el fork, menos responsabilidad para la capa de IPC

--- client/client.c
+++ client/client.c
@@ -16,10 +16,30 @@ void int_handler(int s){
 	exit(0); 
 }
 
+static void send_cmd(char* cmd, int n){
+
+	DB_DATAGRAM *datagram;
+
+	int size = sizeof(DB_DATAGRAM) + n;
+
+	datagram = (DB_DATAGRAM*) calloc(1, size);
+
+	datagram->size = size;
+	datagram->opcode = OP_CMD;
+	strcpy(datagram->dg_cmd, cmd);
+
+	printf("Enviando comando: %s\n", datagram->dg_cmd);
+
+	//DUMP_DATAGRAM(datagram);
+	ipc_send(datagram);
+
+	free(datagram);
+}
+
 int main(int argc, char** argv){
 
 	char buffer[SHMEM_SIZE] = {0};
-	int n, size, err;
+	int n, err;
 	DB_DATAGRAM *datagram;
 
 	printf("Starting client...\n");
@@ -36,31 +56,23 @@ int main(int argc, char** argv){
 	while ((n = read(0, buffer, SHMEM_SIZE)) > 0 ){
 
 		buffer[n-1]=0;
-		size = sizeof(DB_DATAGRAM) + n;
-
-		datagram = (DB_DATAGRAM*) calloc(1, size);
-
-		datagram->size = size;
-		datagram->opcode=OP_CMD;
-		strcpy(datagram->dg_cmd, buffer);
-		datagram->dg_cmd[n] = 0;
-
-		printf("Enviando comando: %s\n", datagram->dg_cmd);
-
-		//STEP-4 Up -> Client
-		ipc_send(datagram);
-
-		free(datagram);
+		
+		send_cmd(buffer, n);
 
 		datagram = ipc_receive();
+		//DUMP_DATAGRAM(datagram);
+
 
 		printf("Respuesta: %s\n", datagram->dg_cmd);
 
+		if(datagram->opcode == OP_EXIT){
+			printf("Exiting...\n");
+			break;
+		}
 
 	}
 
 	printf("Disconnecting\n");
 	ipc_disconnect();
-
 
 }
--- server/server.c
+++ server/server.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "ipc.h"
 
@@ -15,13 +17,53 @@ void int_handler(int s){
 }
 
 
+void serve(int cli_count){
+
+	static char* mensaje = "Mensaje recibido";
+	int n = strlen(mensaje);
+
+	while(1){
+
+		DB_DATAGRAM* dg = ipc_receive();
+		//DUMP_DATAGRAM(dg);
+
+		CLIPRINT("Mensaje recibido: %s\n", dg->dg_cmd);
+
+		// if(strcmp(dg->dg_cmd, "exit") == 0){
+		// 	CLIPRINTE("Exit command received!\n");
+		// 	dg->opcode = OP_EXIT;
+
+		// 	DUMP_DATAGRAM(dg);
+		// 	ipc_send(dg);
+
+		// 	free(dg);
+		// 	break;
+		// }
+
+		dg->size = sizeof(DB_DATAGRAM) + n;
+		memcpy(dg->dg_cmd, mensaje, n);
+
+		CLIPRINT("Enviando respuesta: %s\n", dg->dg_cmd);
+
+		//DUMP_DATAGRAM(dg);
+		ipc_send(dg);
+
+		free(dg);
+
+	}
+
+	CLIPRINTE("Cliente desconectado. Limpiando...\n");
+
+	ipc_disconnect();
+
+	exit(0);
+
+}
+
 int main(int argc, char** argv){
 	
 	int err;
-
-	// char buffer[BUFFER_SIZE];
-	// char c;
-	// int read=0;
+	int pid;
 
 	printf("Starting server...\n");
 
@@ -35,13 +77,41 @@ int main(int argc, char** argv){
 	}
 
 	while(1){
+
 		ipc_accept();
 
-		printf("Cliente aceptado. Esperando mas clientes...\n");
+		switch (pid = fork()){
+
+			case -1:
+				printf("fork failed.\n");
+				exit(1);
+				break;
+
+			case 0:{ /* hijo */
+
+				int cli_count;
+
+				cli_count = ipc_sync();
+
+				serve(cli_count);
+
+				break;
+
+			}
+
+			default:
+	 			
+	 			//En caso de ser necesario, esperamos que el cliente termine de 
+	 			//sincronizar con el servidor para aceptar el proximo cliente.
+				ipc_waitsync();
+
+				printf("Cliente aceptado. Esperando mas clientes...\n");
+
+				break;
+
+		}
 
 	}
 
-
-	
 	return 0;
 }
