5906cc2a31f2dee5a5ee0f0a2a91469f0c2fb3a4
Sebastian Kulesz <sebikul@gmail.com>
2015-09-19 12:17:55 -0300

    semaphore: Nueva librería que contiene las funciones para interactuar con semáforos

---
+++ include/semaphore.h
@@ -0,0 +1,42 @@
+
+#include "ipc.h"
+
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#define PRINT_SEM_VALUES 	printf("SEM_CLIENT: %d\nSEM_SERVER: %d\n", get_sem_val(session->semid, SEM_CLIENT), get_sem_val(session->emid, SEM_SERVER))
+#define PRINT_QSEM_VALUES 	printf("SEM_QUEUE: %d\nSEM_SRV_QUEUE: %d\n", get_sem_val(session->queueid, SEM_QUEUE), get_sem_val(session->queueid, SEM_SRV_QUEUE))
+
+#define SEM_QUEUE			0
+#define SEM_SRV_QUEUE		1
+
+#define SEM_SERVER 			0
+#define SEM_CLIENT 			1
+
+#define WAIT_FOR(sem) 		sem_down(session->semid, sem)
+#define UNBLOCK(sem)		sem_up(session->semid, sem)
+
+#define WAIT_FOR_QUEUE(sem) sem_down(session->queueid, sem)
+#define UNBLOCK_QUEUE(sem) 	sem_up(session->queueid, sem)
+
+
+void sem_init_with_key(int* semid, key_t semkey, int n);
+
+void sem_init(int* semid, int n);
+
+void sem_queue_init(int* queueid, int n);
+
+void sem_set(int semid, int semnum, int val);
+
+void sem_destroy(int semid);
+
+void sem_queue_destroy(int queueid);
+
+void sem_down(int semid, int semnum);
+
+void sem_up(int semid, int semnum);
+
+int get_sem_val(int semid, int semnum );
+
+
+#endif
---
+++ ipc/lib/semaphore.c
@@ -0,0 +1,102 @@
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "semaphore.h"
+
+void sem_init_with_key(int* semid, key_t semkey, int n) {
+
+	//Creamos los semaforos para sincronizar cliente servidor
+	if ((*semid = semget(semkey, n, 0)) >= 0 ) {
+		//printf("Sync semaphores already exist with id: %d\n", shmem.semid);
+		return;
+	}
+
+	if ((*semid = semget(semkey, n, IPC_CREAT | 0666)) == -1 ) {
+		printf("semget failed.\n");
+		exit(1);
+	}
+
+	//printf("Initialized sync semaphores with id %d\n", shmem.semid);
+
+}
+
+
+void sem_init(int* semid, int n) {
+
+	key_t semkey = ftok("../database.sqlite", 0);
+
+	sem_init_with_key(semid, semkey, n);
+
+}
+
+void sem_queue_init(int* queueid, int n) {
+
+	//El id solo usa los 8 bits menos significativos. Seteamos a FF que es la cant maxima de clientes -1 que puede aguantar el servidor
+	key_t queuekey = ftok("../database.sqlite", 0xFF);
+
+	//Creamos el semaforo para la cola de clientes
+	if ((*queueid = semget(queuekey, n, 0)) >= 0 ) {
+		//printf("Queue semaphore already exist with id: %d\n", shmem.queueid);
+		return;
+	}
+
+	if ((*queueid = semget(queuekey, n, IPC_CREAT | 0666)) == -1 ) {
+		printf("semget queue failed.\n");
+		exit(1);
+	}
+
+	//printf("Initialized queue semaphores with id %d\n", shmem.queueid);
+
+}
+
+void sem_set(int semid, int semnum, int val) {
+
+	semctl(semid, semnum, SETVAL, val);
+
+}
+
+void sem_destroy(int semid) {
+
+	//CLIPRINTE("Destroying semaphores...\n");
+
+	//Destruimos los semaforos
+	semctl(semid, 0, IPC_RMID);
+
+}
+
+void sem_queue_destroy(int queueid) {
+
+	semctl(queueid, 0, IPC_RMID);
+
+}
+
+/*
+	Ejecuta down sobre el semaforo
+ */
+void sem_down(int semid, int semnum) {
+
+	struct sembuf sb;
+
+	sb.sem_num = semnum;
+	sb.sem_op = -1;
+	sb.sem_flg = SEM_UNDO;
+	semop(semid, &sb, 1);
+}
+
+/*
+	Ejecuta un up sobre el semaforo
+ */
+void sem_up(int semid, int semnum) {
+
+	struct sembuf sb;
+
+	sb.sem_num = semnum;
+	sb.sem_op = 1;
+	sb.sem_flg = SEM_UNDO;
+	semop(semid, &sb, 1);
+}
+
+int get_sem_val(int semid, int semnum ) {
+	return (semctl(semid, semnum, GETVAL));
+}
