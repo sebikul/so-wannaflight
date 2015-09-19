fa1ce376e5e97b721d918e87eb57a4bc6ded3926
Sebastian Kulesz <sebikul@gmail.com>
2015-09-19 19:42:55 -0300

    client: Mejor manejo de la desconexión

--- client/client.c
+++ client/client.c
@@ -12,22 +12,44 @@
 
 static ipc_session session;
 
-void int_handler(int s) {
-	printf("Limpiando antes de desconectar.\n");
-	ipc_disconnect(session);
-	exit(0);
-}
-
 static void send_cmd(char* cmd, int n) {
 	DB_DATAGRAM *datagram;
 	int size = sizeof(DB_DATAGRAM) + n;
+
 	datagram = (DB_DATAGRAM*) calloc(1, size);
 	datagram->size = size;
 	datagram->opcode = OP_CMD;
+
 	strcpy(datagram->dg_cmd, cmd);
+
 	printf("Enviando comando: %s\n", datagram->dg_cmd);
+
 	ipc_send(session, datagram);
 	free(datagram);
+}
+
+static void send_exit(){
+
+	DB_DATAGRAM *datagram;
+
+	datagram = (DB_DATAGRAM*) malloc(sizeof(DB_DATAGRAM));
+	datagram->size = sizeof(DB_DATAGRAM);
+	datagram->opcode = OP_EXIT;
+
+	printf("Enviando comando EXIT\n");
+
+	ipc_send(session, datagram);
+	free(datagram);
+}
+
+void int_handler(int s) {
+	printf("Limpiando antes de desconectar.\n");
+
+	send_exit();
+
+	ipc_disconnect(session);
+	ipc_free(session);
+	exit(0);
 }
 
 int main(int argc, char** argv) {
@@ -40,6 +62,7 @@ int main(int argc, char** argv) {
 	printf("Iniciando cliente...\n");
 
 	signal(SIGINT, int_handler);
+
 	session = ipc_newsession();
 	err = ipc_connect(session, argc - 1, ++argv);
 
@@ -54,16 +77,22 @@ int main(int argc, char** argv) {
 	while ((n = read(0, buffer, SHMEM_SIZE)) > 0 ) {
 		
 		buffer[n - 1] = 0;
+
+		if(strcmp(buffer, "exit") == 0){
+			send_exit();
+			ipc_disconnect(session);
+			ipc_free(session);
+			exit(0);
+		}
+
 		send_cmd(buffer, n);
 		datagram = ipc_receive(session);
 		printf("Respuesta: %s\n", datagram->dg_cmd);
-		if (datagram->opcode == OP_EXIT) {
-			printf("Saliendo...\n");
-			break;
-		}
+		
 	}
-	printf("Desconectando\n");
+
 	ipc_disconnect(session);
 	ipc_free(session);
+
 	return 0;
 }
