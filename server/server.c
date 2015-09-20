4be430989236f88eb1688f5b97b83df98c160253
Sebastian Kulesz <sebikul@gmail.com>
2015-09-20 02:45:38 -0300

    server: Practicamente terminado el pareo del opcode y construcción de la respuesta
    Falta probar bien la base de datos, y actualizar el cliente para que
    construya un datagrama en base a la entrada del usuario con la función
    y los parámetros requeridos.

--- server/server.c
+++ server/server.c
@@ -31,15 +31,15 @@ void sigpipe_handler(int s) {
 #endif
 
 void serve() {
-	static char* mensaje = "Mensaje recibido";
-
-	int n = strlen(mensaje);
 
 	while (1) {
 
-		DB_DATAGRAM* datagram = ipc_receive(session);
+		int opcode;
+
+		DB_DATAGRAM* datagram, *ans;
+
+		datagram = ipc_receive(session);
 		DUMP_DATAGRAM(datagram);
-		CLIPRINT("Mensaje recibido: %s\n", datagram->dg_cmd);
 
 		if (datagram->opcode == OP_EXIT) {
 			CLIPRINTE("Comando de salida recibido!\n");
@@ -47,30 +47,45 @@ void serve() {
 			free(datagram);
 
 			ipc_disconnect(session);
+			
+			//FIXME: Deberia liberar los recursos, pero no liberar el semaforo de cola
 			//ipc_free(session);
 
 			exit(0);
 		}
 
-		if (strcmp(datagram->dg_cmd, "consultar") == 0) {
-		}
-		if (strcmp(datagram->dg_cmd, "comprar") == 0) {
-		}
-		if (strcmp(datagram->dg_cmd, "cancelar") == 0) {
+		if (datagram->opcode == OP_CONSULT) {
+
+			ans = consult_flights(datagram->dg_origin, datagram->dg_destination);
+
+		} else if (datagram->opcode == OP_PURCHASE) {
+
+			ans = malloc(sizeof(DB_DATAGRAM));
+			ans->size = sizeof(DB_DATAGRAM);
+			ans->opcode = OP_OK;
+			ans->dg_seat = purchase(datagram->dg_flightid);
+
+		} else if (datagram->opcode == OP_CANCEL) {
+
+			ans->opcode = OP_OK;
+
+		} else if (datagram->opcode == OP_PING) {
+
+			int size = sizeof(DB_DATAGRAM) + strlen(datagram->dg_cmd) + 1;
+
+			ans = malloc(size);
+			ans->size = size;
+			ans->opcode = OP_PONG;
+			strcpy(ans->dg_cmd, datagram->dg_cmd);
+
 		}
 
 		free(datagram);
 
-		datagram = malloc(sizeof(DB_DATAGRAM) + n);
-		datagram->size = sizeof(DB_DATAGRAM) + n;
-		datagram->opcode = OP_CMD;
-
-		strcpy(datagram->dg_cmd, mensaje);
-
-		CLIPRINT("Enviando respuesta: %s\n", datagram->dg_cmd);
 		//DUMP_DATAGRAM(datagram);
-		ipc_send(session, datagram);
-		free(datagram);
+		ipc_send(session, ans);
+
+		free(ans);
 	}
 }
 
@@ -89,11 +104,6 @@ int main(int argc, char** argv) {
 
 	printf("Opening database...\n");
 	db_open(DB_PATH);
-
-	// consult_flights(20, 30);
-	// exit(0);
-
-	// //////////////////////////////////////////////
 
 	session = ipc_newsession();
 	err = ipc_listen(session, argc - 1, ++argv);
