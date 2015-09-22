515135c24ca9e98c4442ae680fc2794fe45731d6
alebian <alebezdjian@Alejandros-MacBook-Pro.local>
2015-09-22 20:22:57 -0300

    File-signal skeleton

---
+++ ipc/filesig.c
@@ -0,0 +1,59 @@
+#include "config.h"
+#include "database.h"
+#include "ipc.h"
+#include "semaphore.h"
+
+struct session_t {
+
+};
+
+
+ipc_session ipc_newsession() {
+  ipc_session session = (ipc_session) malloc(sizeof(struct session_t));
+  return (ipc_session)session;
+}
+
+
+int ipc_listen(ipc_session session, int argc, char** args) {
+
+}
+
+
+void ipc_accept(ipc_session session) {
+
+}
+
+
+void ipc_sync(ipc_session session) {
+
+}
+
+
+void ipc_waitsync(ipc_session session) {
+
+}
+
+
+int ipc_connect(ipc_session session, int argc, char** args) {
+
+}
+
+
+int ipc_send(ipc_session session, DB_DATAGRAM* data) {
+
+}
+
+
+DB_DATAGRAM* ipc_receive(ipc_session session) {
+
+}
+
+
+void ipc_disconnect(ipc_session session) {
+
+}
+
+
+void ipc_free(ipc_session session) {
+
+}
