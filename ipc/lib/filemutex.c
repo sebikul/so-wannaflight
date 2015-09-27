96a27690cb8931c758e3e7e2ab5c7378bc45f4a6
Sebastian Kulesz <sebikul@gmail.com>
2015-09-27 19:35:33 -0300

    filemutex: Setear permisos de escritura para poder acceder al lock

--- ipc/lib/filemutex.c
+++ ipc/lib/filemutex.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #include "filemutex.h"
 
@@ -15,11 +16,9 @@ struct fmutex_struct {
 fmutex_t fmutex_new(const char* path) {
 
 	fmutex_t mutex = (fmutex_t) malloc(sizeof(fmutex_t));
-	
+
 	mutex->path = malloc(strlen(path) + 1);
-
 	mutex->fd = -2;
-
 	strcpy(mutex->path, path);
 
 	return mutex;
@@ -31,6 +30,7 @@ void fmutex_enter(fmutex_t mutex) {
 	struct flock lock;
 	int code;
 
+	chmod(mutex->path, S_IWUSR);
 	mutex->fd = open(mutex->path, O_CREAT | O_WRONLY);
 
 	printf("Opening %s, fd: %d, errno: %d\n", mutex->path, mutex->fd, errno);
@@ -59,12 +59,10 @@ void fmutex_leave(fmutex_t mutex) {
 	printf("Unlocking file: %s\n", mutex->path);
 	fcntl(mutex->fd, F_SETLK, &lock);
 	close(mutex->fd);
-
 }
 
 void fmutex_free(fmutex_t mutex) {
 
 	free(mutex->path);
 	free(mutex);
-
 }
